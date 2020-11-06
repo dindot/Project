52ba230b4be1f029e055fd6e9cb2f34e65cc7ddb
Din Bostandzic <dbostand@unix2.lt.ucsc.edu>
2020-11-06 01:24:18 -0800

    added implementation of sieve.c

---
+++ asgn4/sieve.c
@@ -0,0 +1,25 @@
+#include "sieve.h"
+#include "bv.h"
+// The Sieve of Erastothenes
+// Sets bits in BitVector representing prime numbers.
+// Composite numbers are sieved out by clearing bits.
+// v: The BitVector to sieve
+//
+
+void sieve(BitVector *v){
+  bv_set_all_bits(v);
+  bv_clr_bit(v,0);
+  bv_clr_bit(v,1);
+  bv_set_bit(v,2);
+  for (uint32_t i = 2; i < sqrtl(bv_get_len(v)); i+=i){
+	if (bv_get_bit(v,i)){
+         for(uint32_t k =0; (k+i) * i <= bv_get_len(v); k+=1)
+         {
+             bv_clr_bit(v, (k+i) *i);
+
+         }
+    }
+
+}
+return;
+}
