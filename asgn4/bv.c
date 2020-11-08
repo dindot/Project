3a868de8db7b03e883a665d05fd48ee36aae3ce5
Din Bostandzic <dbostand@unix3.lt.ucsc.edu>
2020-11-08 14:59:35 -0800

    completed the implementation, now will clang Wall and need comments and cite profs sieve then done, makefile needed and readme to be complete

--- asgn4/bv.c
+++ asgn4/bv.c
@@ -1,98 +1,85 @@
 #include "bv.h"
-#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-BitVector *bv_create(uint32_t bit_len)
-{
-  BitVector *v = (struct BitVector*)malloc(sizeof(BitVector));
-  if(v == NULL)
-  {
+BitVector *bv_create(uint32_t bit_len) {
+  BitVector *v = (struct BitVector *)malloc(sizeof(BitVector));
+  if (v == NULL) {
     return 0;
   }
-  if (bit_len < 1)
-  {
+  if (bit_len < 1) {
     bit_len = 1;
-  }	
+  }
   v->length = bit_len;
-  v->vector = (uint8_t*)malloc(sizeof(uint32_t) * v->length);
-  if(v->vector == NULL)
-  {
-      return 0; 
+  v->vector = (uint8_t *)malloc(sizeof(uint32_t) * v->length);
+  if (v->vector == NULL) {
+    return 0;
   }
   return v;
 }
 
-void bv_delete(BitVector *v)
-{
-   free(v->vector);
-   free(v);
-   return;
+void bv_delete(BitVector *v) {
+  free(v->vector);
+  free(v);
+  return;
 }
 
-uint32_t bv_get_len(BitVector *v)
-{
+uint32_t bv_get_len(BitVector *v) {
   return v->length;
 }
 
-void bv_set_bit(BitVector *v, uint32_t i)
-{
-  int bucket = i /8;
+void bv_set_bit(BitVector *v, uint32_t i) {
+  int bucket = i / 8;
   int thebit = i % 8;
-//  printf("the bit: %d\n", thebit);
+  //  printf("the bit: %d\n", thebit);
 
   uint8_t thebyte = v->vector[bucket];
-//  printf("the bucket/byte: %d\n", thebyte);
+  //  printf("the bucket/byte: %d\n", thebyte);
 
-  uint8_t shiftbyte =  (00000001<<thebit);
- // printf("the byte to OR original byte: %d\n", shiftbyte);
+  uint8_t shiftbyte = (00000001 << thebit);
+  // printf("the byte to OR original byte: %d\n", shiftbyte);
 
   uint8_t newresult = thebyte | shiftbyte;
- // printf("the new byte with value: %d\n", newresult); 
+  // printf("the new byte with value: %d\n", newresult);
 
   v->vector[bucket] = newresult;
 }
 
-void bv_clr_bit(BitVector *v, uint32_t i)
-{
-  int bucket = i /8;
+void bv_clr_bit(BitVector *v, uint32_t i) {
+  int bucket = i / 8;
   int thebit = i % 8;
- // printf("the bit: %d\n", thebit);
+  // printf("the bit: %d\n", thebit);
 
   uint8_t thebyte = v->vector[bucket];
- // printf("the bucket/byte: %d\n", thebyte);
+  // printf("the bucket/byte: %d\n", thebyte);
 
-  uint8_t shiftbyte =  ~(00000001<<thebit);
-//  printf("the byte to OR original byte: %d\n", shiftbyte);
- 
+  uint8_t shiftbyte = ~(00000001 << thebit);
+  //  printf("the byte to OR original byte: %d\n", shiftbyte);
+
   uint8_t newresult = (thebyte & shiftbyte);
- // printf("the new byte with value: %d\n", newresult); 
+  // printf("the new byte with value: %d\n", newresult);
 
   v->vector[bucket] = newresult;
-
 }
 
-uint8_t bv_get_bit(BitVector *v, uint32_t i)
-{
+uint8_t bv_get_bit(BitVector *v, uint32_t i) {
 
- int bucket = i /8;
+  int bucket = i / 8;
   int thebit = i % 8;
- uint8_t thebyte = v->vector[bucket];
+  uint8_t thebyte = v->vector[bucket];
 
-      uint8_t shiftbyte =  (00000001<<thebit);
-      uint8_t newresult = thebyte &  shiftbyte;
-     uint8_t valueinbit = newresult >> thebit;
-     //printf("valueinbit: %d", valueinbit);
-     //uint8_t extractedval = valueinbit & 00000001;
- //return extractedval;
- return valueinbit;
+  uint8_t shiftbyte = (00000001 << thebit);
+  uint8_t newresult = thebyte & shiftbyte;
+  uint8_t valueinbit = newresult >> thebit;
+  //printf("valueinbit: %d", valueinbit);
+  //uint8_t extractedval = valueinbit & 00000001;
+  //return extractedval;
+  return valueinbit;
 }
 
-void bv_set_all_bits(BitVector *v)
-{
-  for(int i = 0; i < v->length; i++)
-  {
-    bv_set_bit(v,i);
+void bv_set_all_bits(BitVector *v) {
+  for (uint32_t i = 0; i < v->length; i++) {
+    bv_set_bit(v, i);
   }
-
 }
--- asgn4/sequence.c
+++ asgn4/sequence.c
@@ -1,11 +1,11 @@
-#include "sieve.h"
 #include "bv.h"
-#include <stdio.h>
-#include <stdbool.h>
-#include <math.h>
-#include <stdlib.h>
+#include "sieve.h"
 #include <getopt.h>
+#include <math.h>
+#include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #define OPTIONS "spn:"
 int fib(int num);
@@ -15,19 +15,18 @@ bool is_mersenne(int current);
 bool is_lucas(int current);
 bool is_fib(int current);
 void prime_printer(BitVector *bv);
-char* decimal_to_basex(int num, int base);
-bool isPalindrome (char* s);
-void palindrome_primeprint(BitVector *bv);
+char *decimal_to_basex(int num, int base);
+bool isPalindrome(char *s);
+void palindrome_primeprint(BitVector *bv, int b);
 
-int main(int argc, char **argv)
-{
-bool s, p, n;
-int default_prime = 0;
-s = p = n = false;
-char *next_input = NULL;
-int c = 0;
+int main(int argc, char **argv) {
+  bool s, p, n;
+  int default_prime = 0;
+  s = p = n = false;
+  char *next_input = NULL;
+  int c = 0;
 
-while ((c = getopt(argc, argv, OPTIONS))
+  while ((c = getopt(argc, argv, OPTIONS))
          != -1) // parse user input to get options input
 
   {
@@ -51,266 +50,221 @@ while ((c = getopt(argc, argv, OPTIONS))
     }
   }
 
-if (argc == 1) { // user must enter an option in order for program to run
+  if (argc == 1) { // user must enter an option in order for program to run
     puts("Error: no arguments supplied!");
     return -1;
   }
 
+  if (s == true) {
+    if (n == false) {
+      default_prime = 1000;
+    }
 
- if(s == true)
-{
-if(n == false)
-{ 
-   default_prime = 1000;
- 
- }
-
- BitVector *bv = bv_create(default_prime);
-sieve(bv);
-prime_printer(bv);
-
-
-
-}
-
-if(p == true)
-{
-if(n == false)
-{
-   default_prime = 1000;
-
- }
-
-BitVector *bv2 = bv_create(default_prime);
-sieve(bv2);
- palindrome_primeprint(bv2);
-}
-
-
-
-char* x = decimal_to_basex(43, 2);
-      printf("%s", x);
-
-
-
-return 0;
-}
-
-void prime_printer(BitVector *bv)
-{
-for (int i = 2; i < bv->length; i++)
-{
-   bool isfib = is_fib(i);
-   bool islucas = is_lucas(i);
-   bool ismers = is_mersenne(i);
-   if(bv_get_bit(bv,i) == 1)
-   {
-       if (ismers == 1 && islucas == 1 && isfib == 1)
-       {
-          printf("%d: prime, mersenne, lucas, fibonacci\n", i);
-       }
-       if (islucas == 1 && isfib == 0 && ismers == 0)
-       {
-       printf("%d: prime, lucas\n", i);
-       }
-       if (islucas == 0 && isfib == 1 && ismers == 0)
-       {
-       printf("%d: prime, fibonacci\n", i);
-       }
-        if (islucas == 0 && isfib == 0 && ismers == 1)
-       {
-       printf("%d: prime, mersenne\n", i);
-       }
-       if (islucas == 0 && isfib == 1 && ismers == 1)
-       {
-       printf("%d: prime, mersenne, fibonacci\n", i);
-       }
-      if (islucas == 1 && isfib == 0 && ismers == 1)
-       {
-       printf("%d: prime, mersenne, lucas\n", i);
-       }
-       if (islucas == 1 && isfib == 1 && ismers ==0 )
-       {
-       printf("%d: prime, lucas, fibonacci\n", i);
-       }
-       if (islucas == 0 && isfib == 0 && ismers ==0)
-       {
-       printf("%d: prime\n", i);
-       }  
+    BitVector *bv = bv_create(default_prime);
+    sieve(bv);
+    prime_printer(bv);
+    bv_delete(bv);
   }
-} 
 
+  if (p == true) {
+    if (n == false) {
+      default_prime = 1000;
+    }
 
-
+    BitVector *bv2 = bv_create(default_prime);
+    sieve(bv2);
+    palindrome_primeprint(bv2, 2);
+    printf("\n");
+    palindrome_primeprint(bv2, 9);
+    printf("\n");
+    palindrome_primeprint(bv2, 10);
+    printf("\n");
+    palindrome_primeprint(bv2, 12);
+    bv_delete(bv2);
+  }
+  return 0;
 }
 
+void prime_printer(BitVector *bv) {
+  for (uint32_t i = 2; i < bv->length; i++) {
+    bool isfib = is_fib(i);
+    bool islucas = is_lucas(i);
+    bool ismers = is_mersenne(i);
+    if (bv_get_bit(bv, i) == 1) {
+      if (ismers == 1 && islucas == 1 && isfib == 1) {
+        printf("%d: prime, mersenne, lucas, fibonacci\n", i);
+      }
+      if (islucas == 1 && isfib == 0 && ismers == 0) {
+        printf("%d: prime, lucas\n", i);
+      }
+      if (islucas == 0 && isfib == 1 && ismers == 0) {
+        printf("%d: prime, fibonacci\n", i);
+      }
+      if (islucas == 0 && isfib == 0 && ismers == 1) {
+        printf("%d: prime, mersenne\n", i);
+      }
+      if (islucas == 0 && isfib == 1 && ismers == 1) {
+        printf("%d: prime, mersenne, fibonacci\n", i);
+      }
+      if (islucas == 1 && isfib == 0 && ismers == 1) {
+        printf("%d: prime, mersenne, lucas\n", i);
+      }
+      if (islucas == 1 && isfib == 1 && ismers == 0) {
+        printf("%d: prime, lucas, fibonacci\n", i);
+      }
+      if (islucas == 0 && isfib == 0 && ismers == 0) {
+        printf("%d: prime\n", i);
+      }
+    }
+  }
+}
 
-
-int fib(int num)
-{
-  if (num == 0 || num == 1)
-  {
+int fib(int num) {
+  if (num == 0 || num == 1) {
     return num;
-  }
-  else
-  {
-    return fib(num-1) + fib(num-2);
-   
+  } else {
+    return fib(num - 1) + fib(num - 2);
   }
 }
 
-
-bool is_fib(int current)
-{
+bool is_fib(int current) {
   bool found_fib = false;
-  for(int i =0; i<20; i++)
-  {
+  for (int i = 0; i < 20; i++) {
     int fibnum = fib(i);
-    if (fibnum == current)
-    {
+    if (fibnum == current) {
       found_fib = true;
     }
   }
 
- return found_fib;
+  return found_fib;
 }
 
-int lucas(int num)
-{
-  if (num == 0)
-  {
+int lucas(int num) {
+  if (num == 0) {
     return 2;
-  }
-  else if (num == 1)
-  {
+  } else if (num == 1) {
     return num;
-  }
-  else
-  {
-    return lucas(num-1) + lucas(num-2);
-
+  } else {
+    return lucas(num - 1) + lucas(num - 2);
   }
 }
 
-bool is_lucas(int current)
-{
+bool is_lucas(int current) {
   bool found_lucas = false;
-  for(int i =0; i<20; i++)
-  {
+  for (int i = 0; i < 20; i++) {
     int lucasnum = lucas(i);
-    if (lucasnum == current)
-    {
+    if (lucasnum == current) {
       found_lucas = true;
     }
   }
 
- return found_lucas;
+  return found_lucas;
 }
 
-int mersenne(int num)
-{
- int val = 0;
- if(num == 2)
-  { 
-     return 3;
-   }
-  if (num%2 != 0)
-  {
+int mersenne(int num) {
+  int val = 0;
+  if (num == 2) {
+    return 3;
+  }
+  if (num % 2 != 0) {
 
-    val= (pow(2,num)-1);
-   }
-return val;
- }
+    val = (pow(2, num) - 1);
+  }
+  return val;
+}
 
-
-bool is_mersenne(int current)
-{
- bool found_mersenne = false;
-  for(int i =0; i< 20; i++)
-  {
+bool is_mersenne(int current) {
+  bool found_mersenne = false;
+  for (int i = 0; i < 20; i++) {
     int mersennenum = mersenne(i);
-    if (mersennenum == current)
-    {
-      found_mersenne= true;
+    if (mersennenum == current) {
+      found_mersenne = true;
     }
-
+  }
+  return found_mersenne;
 }
-return found_mersenne;
+
+char *decimal_to_basex(int num, int base) {
+  char *base_string = NULL;
+  char c;
+  char *thestring = NULL;
+  char string[8];
+  char newstring[8];
+  int counter = 0;
+  if (num / base == 0) {
+    if (num % base == 10) {
+      return "A";
+    } else if (num % base == 11) {
+      return "B";
+    } else if (num % base == 12) {
+      return "C";
+    } else if (num % base == 13) {
+      return "D";
+    } else if (num % base == 14) {
+      return "E";
+    } else if (num % base == 15) {
+      return "F";
+    }
+  }
+  if (num == 2 && base == 2) {
+    return "1";
+  } else if (num == 2 && base != 2) {
+    return "2";
+  }
+  while (num > 0) {
+    int x = num;
+    num = (num / base);
+    int remainder = x % base;
+    c = remainder + '0';
+    string[counter] = c;
+    // printf("string is: %s", string);
+
+    counter++;
+  }
+  string[counter] = '\0';
+  thestring = string;
+  //     printf("string is: %s", thestring);
+  counter--;
+  for (int i = counter, k = 0; i >= 0; i--, k++) {
+    newstring[k] = string[i];
+    if (i == 0) {
+      newstring[k + 1] = '\0';
+    }
+  }
+
+  base_string = newstring;
+  return base_string;
 }
 
+bool isPalindrome(char *s) {
 
+  bool f = true;
+  int length = strlen(s);
 
-char* decimal_to_basex(int num, int base)
-{
- char *base_string = NULL;
- char c;
- char string[8] ;
- char newstring[8];
-int counter = 0;
-  while (num > 0)
-  {
-      int x = num;
-      num= (num/ base);
-      int remainder = x % base;
-      c = remainder + '0';
-      string[counter] = c;
-     
-      counter++; 
-      }
-     
-      counter--;
-     for (int i = counter, k = 0; i >=0; i--, k++)
-{
-  newstring[k] = string[i];
-}
-  base_string = newstring;           
-           return base_string;
- }
+  for (int i = 0; i < length; i++) {
 
-
-
-bool isPalindrome (char* s){
-
-bool f = true;
-int length = strlen(s);
-
-for(int i =0; i< length; i++)
-{
-
-    if(s[i] != s[length -(i+1)])
-    {
+    if (s[i] != s[length - (i + 1)]) {
       f = false;
-       break;
-         }
-          
-          }
-         return f;
-         }
-
-
-
-void palindrome_primeprint(BitVector *bv)
-{
-int base = 2;
-printf("Base %d\n", base);
-puts("---- --\n");
-for (int i = 2; i < bv->length; i++)
-{
-  
-   if(bv_get_bit(bv,i) == 1)
-   {
-  char *newbase =  decimal_to_basex(3, base);
-  printf("the new base is%s\n", newbase);
-  bool palindrome = isPalindrome(newbase);
-  
-  if (palindrome == 1)
-  {  
-   // printf("%d = %s\n", i, newbase);
-   }
-   }
-
+      break;
+    }
+  }
+  return f;
 }
 
+void palindrome_primeprint(BitVector *bv, int b) {
+  int base = b;
+  printf("Base  %d\n", base);
+  puts("---- --");
+  for (uint32_t i = 2; i < bv->length; i++) {
 
+    if (bv_get_bit(bv, i) == 1) {
+      char *newbase = decimal_to_basex(i, base);
+      // printf("the new base is%s\n", newbase);
+      bool palindrome = isPalindrome(newbase);
 
+      if (palindrome == 1) {
+        printf("%d = %s\n", i, newbase);
+      }
+    }
+  }
 }
