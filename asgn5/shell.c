1343b499ddfc29a529ff6810be7a680a5c855987
Din Bostandzic <dbostand@unix3.lt.ucsc.edu>
2020-11-12 16:52:54 -0800

    added shell.c

---
+++ asgn5/shell.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+
+int *gap(int n)
+{
+  static int arr[20] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};  // store gap intervals
+  int i =0;
+  while (n >1)
+  {
+    if(n<=2)
+    {
+      n = 1;
+      arr[i] = n;
+      i++;
+    }
+    else
+    {
+        n = (5*n)/11;  // gap equation used
+        arr[i] = n;
+        i++;
+    }
+
+    }
+   return arr;
+}
+
+void shell_sort(int arr[], int length, int arr2[])
+{
+  static int i = 0;
+  static int step = 0;  // retain values of step and access in array of gap interval
+  for(; step=arr2[i]; i++)
+   {
+     for(int i = step; i< length; i++)
+     {
+        for(int j = i; j>= step; j-=step)
+        {
+          if(arr[j] < arr[j-step])  // after stepping through gaps swap if not sorted
+          {
+             int temp = arr[j];
+  	     arr[j] = arr[j-step];
+             arr[j-step] = temp;
+          }
+        }
+     }
+
+   }
+}
