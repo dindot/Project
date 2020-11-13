3ea3a47a62ee7b425c9c4a86acedd9b8832f59e8
Din Bostandzic <dbostand@unix3.lt.ucsc.edu>
2020-11-12 18:55:30 -0800

    added binary.c

---
+++ asgn5/binary.c
@@ -0,0 +1,38 @@
+#include "binary.h"
+
+void binary_insertion_sort(int arr[], int length)
+{
+  for(int i = 1; i < length; i++)
+   {
+       int value = arr[i];
+       int left = 0;
+        int right = i;
+       
+       while(left<right)
+       {
+         int mid = left + ((right-left)/2);  // find index of element in mid of array
+         if(value>= arr[mid])
+         {
+
+             left = mid +1;  // start the left next one over, since value is right half
+         }
+         else
+         {
+            right = mid;  // make right the mid index because will be looking in left half
+         } 
+
+       } 
+    
+    for(int j = i; j>left; j--)
+    {
+       int temp = arr[j-1];
+       arr[j-1] = arr[j];
+       arr[j] = temp;
+     }
+     }  
+
+
+
+
+
+}
