9e2428b5d0b9763604020e6c666c73e942732e3c
Din Bostandzic <dbostand@unix3.lt.ucsc.edu>
2020-11-12 16:28:29 -0800

    added bubble header

---
+++ asgn5/bubble.h
@@ -0,0 +1,9 @@
+#ifndef __BUBBLE_H__
+#define __BUBBLE_H__
+
+// bubble sort algorithm takes in an integer array with integer length
+// and proceeds to sort by going through element comparing it with the next
+// element, for each element and swapping them to sort the array in asc order
+void bubble_sort(int arr[], int length);
+
+#endif
