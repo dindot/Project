e71647e6f5d721f93df8590cdba5808f019fade7
Din Bostandzic <dbostand@unix3.lt.ucsc.edu>
2020-11-18 22:17:33 -0800

    finished implementation of bf.c, working

--- asgn6/bf.c
+++ asgn6/bf.c
@@ -1,13 +1,15 @@
 #include "bf.h"
+#include "speck.h"
 #include <stdlib.h>
 
+#define BFSIZE 1048576
 //
 // Constructor for a BloomFilter.
 //
 // size: The number of entries in the BloomFilter.
 //
 BloomFilter *bf_create(uint32_t size){
-  Bloomfilter *bf = (BloomFilter *)malloc(sizeof(BloomFilter));
+  BloomFilter *bf = (BloomFilter *)malloc(sizeof(BloomFilter));
   if (bf){
     bf->primary[0] = 0xfc28ca6885711cf7;
     bf->primary[1] = 0x2841af568222f773;
@@ -28,7 +30,28 @@ void bf_delete(BloomFilter *bf)
 
 void bf_insert(BloomFilter *bf, char *key)
 {
-  hash();
+  uint32_t index = 0;
+  index = hash(bf->primary, key) % BFSIZE;  // apply the 3 salts to the hash, set bit to 1 of index
+  bv_set_bit(bf->filter, index);
+  index = hash(bf->secondary, key) % BFSIZE;
+  bv_set_bit(bf->filter, index);
+  index = hash(bf->tertiary, key) % BFSIZE;
+  bv_set_bit(bf->filter, index);
+}
 
+bool bf_probe(BloomFilter *bf, char *key)
+{
+ uint32_t index1 = hash(bf->primary, key) % BFSIZE;  // get indices when applied 3 salts to hash
+ uint32_t index2 = hash(bf->secondary, key) % BFSIZE;
+ uint32_t index3 = hash(bf->tertiary, key) % BFSIZE;
+ 
+ // check to see if each bit is turned on, then its an oldspeak word
+ if(bv_get_bit(bf->filter, index1) == 1 && bv_get_bit(bf->filter, index2) == 1 && bv_get_bit(bf->filter, index3) == 1)
+  {
+    return 1;
+  }
+ 
+return 0;
 
 }
+
--- asgn6/tester.c
+++ asgn6/tester.c
@@ -3,8 +3,11 @@
 
 int main(void)
 {
-
-
-
+ bool inbf = 0;
+ BloomFilter *bf = bf_create(1048576);  // max size of 20^2
+ bf_insert(bf, "potato");
+ bf_insert(bf, "reddy");
+ inbf = bf_probe(bf, "potato");
+ printf("%d", inbf);
 return 0;
 }
