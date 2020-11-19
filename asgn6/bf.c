abb4411d2627b254c3f0609ecc127201624bb6fb
Din Bostandzic <dbostand@unix3.lt.ucsc.edu>
2020-11-18 21:23:27 -0800

    added bf.c

---
+++ asgn6/bf.c
@@ -0,0 +1,22 @@
+#include "bf.h"
+#include <stdlib.h>
+
+//
+// Constructor for a BloomFilter.
+//
+// size: The number of entries in the BloomFilter.
+//
+BloomFilter *bf_create(uint32_t size){
+  Bloomfilter *bf = (BloomFilter *)malloc(sizeof(BloomFilter));
+  if (bf){
+    bf->primary[0] = 0xfc28ca6885711cf7;
+    bf->primary[1] = 0x2841af568222f773;
+    bf->secondary[0] = 0x85ae998311115ae3;
+    bf->secondary[1] = 0xb6fac2ae33a40089;
+    bf->tertiary[0] = 0xd37b01df0ae8f8d0;
+    bf->tertiary[1] = 0x911d454886ca7cf7;
+    bf->filter = bv_create(size);
+    return bf;
+}
+ return (BloomFilter*)NIL;
+}
