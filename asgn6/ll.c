10e3a172bd7916f2e3bcea34f1956c3e0bf24476
Din Bostandzic <dbostand@unix4.lt.ucsc.edu>
2020-11-20 02:47:03 -0800

    finished linked list, time for hash table and parser and what not

--- asgn6/ll.c
+++ asgn6/ll.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 static int seek = 0;
 static int numlinks = 0;
+bool move_to_front;
 
 ListNode *ll_node_create(HatterSpeak *gs)
 {
@@ -30,27 +31,17 @@ free(n);
 
 void ll_delete(ListNode *head)
 {
-//  ListNode *prevnode = head->next;
- // ListNode *curr = head;
- // while(curr->next != NIL)
- // { 
-  // curr = (head->next->next);
-  // ll_node_delete(prevnode);
-  // head->next = prevnode->next;
- // }
- // ll_node_delete(curr);
- //
- 
  while(head->next != NIL)
  {
      ll_node_delete(head->next);
       head->next = (head->next)->next;
       
-   
-    
   }
  ll_node_delete(head);
 }
+
+
+
 
 ListNode *ll_insert(ListNode **head, HatterSpeak *gs)
 {
@@ -58,24 +49,117 @@ ListNode *ll_insert(ListNode **head, HatterSpeak *gs)
 ListNode *node = ll_node_create(gs);
 
 //ListNode **prev = head;
-(*head)->next = node;
-//head = &node;
+//
+node->next = *head;
+
+//LinkedNode **current = &head;
+
+*head = node;
+
 return *head;  // move the head to point to the new node made, return it
 }
 
+
+
 ListNode* ll_lookup(ListNode **head, char *key)
 {
- ++seek;
+//printf("%d", move_to_front);
+  ++seek;
+  printf("     %s   ", (*head)->gs->oldspeak);
+ ListNode * storehead = *head; 
+  while((*head)->next != NIL)
+  {
+    ++numlinks;
+    *head = (*head)->next;
+      printf("dfd     %s   ", (*head)->gs->oldspeak);
+      printf("%s",key);
+    if ((*head)->gs->oldspeak == key && move_to_front == 0)
+    {
+       return *head;
+    }
+    
+  else if ((*head)->gs->oldspeak == key && move_to_front == 1)
+    {
+      HatterSpeak *keydataswap = (*head)->gs;
+     //char* key = (*head)->gs->oldspeak;
+     //char* data = (*head)->gs->hatterspeak;
+    
+     while((*head)->next != NIL)
+     {
+      ++numlinks;
+         *head = (*head)->next;
+    printf("   checking move to  %s   ", (*head)->gs->oldspeak);
+	 }
+        printf("   asdf  %s   ", (*head)->gs->oldspeak);
+    
+
+       if((*head)->next == NIL){
+      HatterSpeak *tempkeydata = (*head)->gs;
+      //char* tempkey =(*head)->gs->oldspeak;
+      //char* tempdata = (*head)->gs->hatterspeak;
+      // (*head)->gs->oldspeak = key;
+     // (*head)->gs->hatterspeak = data;
+     // tempkey = key;
+    //  data = tempdata;
+       (*head)->gs = keydataswap;
+       keydataswap = tempkeydata;
+      //return *head; // this is the swapped node (moved it to front)
+      return storehead;  // returning to be able to go thru entire list to check
+      }
+     }
+
+    }
+   return NIL;
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ++seek;
  while( (*head)->next != NULL)
   {
    head = &((*head)->next);
    ++numlinks;
+
    if( (*head)->gs->oldspeak == key)
    {
-     return *head;
+     HatterSpeak *swapdatakey = (*head)->gs;
+     while(move_to_front == 1)
+     {
+      if((*head)->next != NULL) // check if its alreaedy the item at start of list, no need to swap
+	{
+       head = &((*head)->next);
+         if((*head)->next == NULL)
+          {
+		HatterSpeak *temp = (*head)->gs;
+		(*head)->gs = swapdatakey;
+		swapdatakey = temp;
+          }
+    
+         }
+     
+      else
+     {
+       
+       return *head;
+    }
+ move_to_front = 0;  
+}
+
+ return *head;
    }
 
   }
-return *head;
+return NIL;
+*/
 
-}
--- asgn6/tester.c
+++ asgn6/tester.c
@@ -15,27 +15,72 @@ int main(void)
  inbf= bf_probe(bf, "adam");
 printf("%d", inbf);
 //bf_delete(bf);
-HatterSpeak *gs = (HatterSpeak*)malloc(sizeof(HatterSpeak));
-gs->oldspeak = "hrmy";
-gs->hatterspeak = "benny";
-ListNode* node = ll_node_create(gs);
-printf("node %s", node->gs->oldspeak);
+HatterSpeak *ds = (HatterSpeak*)malloc(sizeof(HatterSpeak));
+ds->oldspeak = "hrmy";
+ds->hatterspeak = "benny";
+ListNode* node = ll_node_create(ds);
+//printf("node %s", node->gs->oldspeak);
+
+//ListNode *initial = ll_insert(&node, ds);     /// need these 2 lines to can print up to hrmy
+//printf("head is %s\n", initial->gs->oldspeak);
 
 HatterSpeak *ss = (HatterSpeak*)malloc(sizeof(HatterSpeak));
 ss->oldspeak = "redd";
 ss->hatterspeak = "defef";
 ListNode *head = ll_insert(&node, ss);
-printf("head is %s", head->gs->oldspeak);
+printf("head is %s\n", head->gs->oldspeak);
 
-ListNode* foundnode =  ll_lookup(&head, "redd");
-printf("found %s", foundnode->gs->oldspeak);
+HatterSpeak *bb = (HatterSpeak*)malloc(sizeof(HatterSpeak));
+bb->oldspeak = "john";
+bb->hatterspeak = "pierce";
+ListNode *add = ll_insert(&node, bb);
+printf("head is %s\n", add->gs->oldspeak);
 
-//free(node->gs);
-ll_delete(node);
-if(node->gs->oldspeak == NIL)
+HatterSpeak *ed = (HatterSpeak*)malloc(sizeof(HatterSpeak));
+ed->oldspeak = "mart";
+ed->hatterspeak = "aser";
+ListNode *hety = ll_insert(&node, ed);
+printf("head is %s\n", hety->gs->oldspeak);
+
+ move_to_front =1 ;
+
+
+printf("\n passsing nodes %s\n", node->gs->oldspeak);
+ListNode* foundnode =  ll_lookup(&node,"john");
+
+if (foundnode != NIL)
 {
-node = NIL;
-printf("\nthe entire list has been deleted"); 
+printf("\nfound %s", foundnode->gs->oldspeak);
 }
+else
+{
+printf("\nNot in list");
+}
+
+
+while(foundnode != NIL)
+{
+printf("\n nodes %s\n", foundnode->gs->oldspeak);  //show list with searched found value moved to front of list
+foundnode = foundnode->next;
+}
+
+
+//}
+//if(move_to_front == 1)
+//{
+ //  printf("moved to front of list: %s", foundnode->gs->oldspeak);
+//}
+//}
+
+
+//printf("\n did i delete%s", head->gs->oldspeak);
+//printf("\n did i delete%s", node->gs->oldspeak);
+//*free(node->gs);
+//ll_delete(node);
+///if(node->gs->oldspeak == NIL)
+//{
+//node = NIL;
+///printf("\nthe entire list has been deleted"); 
+//}*/
 return 0;
 }
