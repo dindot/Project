5caec7f963b606ab3c6057f9ccffab183954b5e9
Din Bostandzic <dbostand@unix2.lt.ucsc.edu>
2020-12-11 05:56:06 -0800

    having difficulties encoding due to complete buffer being printed out, only the magic number and protection is visible with xxd

--- asgn7/io.c
+++ asgn7/io.c
@@ -14,7 +14,8 @@ static uint8_t word_buff[4096];
 
 int read_bytes(int infile, uint8_t *buf, int to_read)
 {
-int bytes = read(infile, buf, to_read);
+int offset = 0;
+int bytes = read(infile, buf+offset, to_read);
 int counter = 0;
 
 if(bytes == 0)
@@ -24,7 +25,7 @@ return to_read;
 
 counter += bytes;
 
-int offset = to_read - bytes;
+offset = to_read - bytes;
 while(bytes != 0)
 {
 bytes = read(infile, buf+offset, offset);
@@ -36,8 +37,8 @@ return counter;
 
 int write_bytes(int outfile, uint8_t *buf, int to_write)
 {
-
-int bytes = write(outfile, buf, to_write);
+int offset = 0;
+int bytes = write(outfile, buf+offset, to_write);
 int counter = 0;
 
 if(bytes == 0)
@@ -47,7 +48,7 @@ return to_write;
 
 counter += bytes;
 
-int offset = to_write - bytes;
+offset = to_write - bytes;
 while(bytes != 0)
 {
 bytes = write(outfile, buf+offset, offset);
@@ -73,11 +74,6 @@ if (header->magic == MAGIC) {
 
 void write_header(int outfile, FileHeader *header) {
 
- //struct stat srcstats;
- //fstat(outfile, &srcstats);
-
-//header->magic = MAGIC;
-//header->protection = srcstats.st_mode;
 write_bytes(outfile, (uint8_t*)header, sizeof(FileHeader));
 }
 
@@ -90,20 +86,13 @@ bool read_sym(int infile, uint8_t *sym) {
 bool toread = 1;
 static int i = 0;
 
-// change this i here later maybe
-int bytes = read_bytes(infile, readbuf+i, sizeof(readbuf));
+int bytes = read_bytes(infile, readbuf, sizeof(readbuf));
 
 if(bytes == 0)
 {
-toread = 0;
-}
-//printf("\n bytes are %d", bytes);
-
-
-if(bytes == 4096)
-{
 *sym = readbuf[i];
 ++i;
+
 
 if(i == 4096)
 {
@@ -114,14 +103,16 @@ for(int i = 0; i < 4096; i++)
 readbuf[i] = 0;
 }
 
-bytes = read_bytes(infile, readbuf+i, sizeof(readbuf));
+bytes = read_bytes(infile, readbuf, sizeof(readbuf));
 
 if(bytes==0){
    toread = 0;
 }
 }
 
+
 }
+
 if(bytes < 4096)
 {
 *sym = readbuf[i];
@@ -135,56 +126,13 @@ return toread;
 }
 
 
-
-
-/*  static bool notblock = 0;
-  static bool block = 0;
-  static int i = 0;
-  static int x = 0;
-  int bytes = 0;
-  bool toread = 0;
-  static int endbuf = 4096;
-
-  bytes = read(infile, readbuf + i, sizeof(readbuf));
-
-  if (bytes == 4096 || block == 1) {
-    block = 1;
-    if (i < bytes) {
-      toread = 1;
-      *sym = readbuf[i];
-      i++;
-      if (readbuf[i] == (uint8_t)NULL)
-        toread = false;
-    }
-  }
-
-  if (bytes < 4096 && bytes != 0) {
-    endbuf = bytes;
-  }
-
-  if (bytes < 4096 || notblock == 1) {
-    notblock = 1;
-    if (x < endbuf) {
-      toread = 1;
-      *sym = readbuf[x];
-      x++;
-      if (readbuf[x] == (uint8_t)NULL)
-        toread = false;
-    }
-  }
-*/
-//  return toread;
-//}
-
-
-
 // making the compressed versiob, putting into outfile, use write buffer, track w bits write
 void buffer_pair(int outfile, uint16_t code, uint8_t sym, uint8_t bitlen) {
  // static int bits_written = 0;
-  int minbitscurrcode = (log2(code)) + 1;
-  int pad = bitlen - minbitscurrcode;
-  printf("\n the pad is : %d", pad);
-  printf("\n the sym: %d", sym);
+//  int minbitscurrcode = (log2(code)) + 1;
+//  int pad = bitlen - minbitscurrcode;
+  //printf("\n the pad is : %d", pad);
+ // printf("\n the sym: %d", sym);
  
   
  for(int i = 0; i < bitlen; i++)
@@ -202,7 +150,7 @@ if(the_bitval == 1)
 {
 uint16_t writebyte = writebuf[curr_byt];
 uint16_t bit = writebyte | (00000001 << curr_bit);
-printf("\n the supposed byte: %d", bit);
+//printf("\n the supposed byte: %d", bit);
 writebuf[curr_byt] = bit;
 ++bits_written;
 }
@@ -210,7 +158,7 @@ else if(the_bitval == 0)
 {
 uint16_t writebyte = writebuf[curr_byt];
 uint16_t bit = writebyte & (00000001 << curr_bit);
-printf("\n the supposed byte: %d", bit);
+//printf("\n the supposed byte: %d", bit);
 writebuf[curr_byt] |= bit;
 ++bits_written;
 }
@@ -218,7 +166,7 @@ writebuf[curr_byt] |= bit;
 //printf("\n at the bit: %d", curr_bit);
 
 }
-printf("\n the final byte: %d", writebuf[0]);
+//printf("\n the final byte: %d", writebuf[0]);
   
 
 
@@ -229,7 +177,7 @@ if(bits_written == 4096 * 8)
 write_bytes(outfile, writebuf, sizeof(writebuf));
 
 }
-printf("\n bits wrote: %d", bits_written);
+//printf("\n bits wrote: %d", bits_written);
 uint8_t curr_byt = bits_written / 8;
 uint8_t curr_bit = bits_written % 8;
 uint8_t the_bitval = sym & (00000001 <<((curr_bit-curr_bit)+i ));
@@ -238,7 +186,7 @@ if(the_bitval == 1)
 {
 uint8_t writebyte = writebuf[curr_byt];
 uint8_t bit = writebyte | (00000001 << curr_bit);
-printf("\n the supposed byte: %d", bit);
+//printf("\n the supposed byte: %d", bit);
 writebuf[curr_byt] = bit;
 ++bits_written;
 }
@@ -246,13 +194,13 @@ else if(the_bitval == 0)
 {
 uint8_t writebyte = writebuf[curr_byt];
 uint8_t bit = writebyte & (00000001 << curr_bit);
-printf("\n the supposed byte: %d", bit);
+//printf("\n the supposed byte: %d", bit);
 writebuf[curr_byt] |= bit;
 ++bits_written;
 }
 
 
-printf("\n the final byte: %d", writebuf[1]);
+//printf("\n the final byte: %d", writebuf[1]);
 }
 }
 
@@ -289,14 +237,14 @@ if(the_bitval == 1)
 {
 uint16_t bit = *code | (00000001 << curr_bit);
 *code = bit;
-printf("\n *the supposed byte: %d", bit);
+//printf("\n *the supposed byte: %d", bit);
 ++bits_read;
 }
 else if(the_bitval == 0)
 {
 uint16_t bit = *code & (00000001 << curr_bit);
 *code = bit;
-printf("\n *the supposed byte: %d", bit);
+//printf("\n *the supposed byte: %d", bit);
 ++bits_read;
 }
 
@@ -307,7 +255,7 @@ if(*code == STOP_CODE)
 moreread = 0;
 return moreread;
 }
-printf("\n read code: %d", *code);  
+//printf("\n read code: %d", *code);  
 
 
 
@@ -328,14 +276,14 @@ if(the_bitval == 1)
 {
 uint8_t bit = *sym | (00000001 << ((curr_bit-curr_bit)+i ));
 *sym = bit;
-printf("\n *the supposed byte: %d", bit);
+//printf("\n *the supposed byte: %d", bit);
 ++bits_read;
 }
 else if(the_bitval == 0)
 {
 uint8_t bit = *sym & (00000001 << ((curr_bit-curr_bit)+i ));
 *sym = bit;
-printf("\n *the supposed byte: %d", bit);
+//printf("\n *the supposed byte: %d", bit);
 ++bits_read;
 }
 
